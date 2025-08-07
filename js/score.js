761be0499eeb06e22318bb21007e37e9f73fde36
POKEMONGD24 <stojkovicrobin@gmail.com>
2025-08-07 22:41:03 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -8,7 +8,7 @@ export function score(rank, percent, minPercent) {
         return 0
        }
 
-    let score = ((-0.22371358 * (rank)) + 100.22371358) * ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    let score = ((-24.9975 * (rank-1, 0.4)) + 200) * ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     score = Math.max(0, score);
 
     if (percent != 100) {
