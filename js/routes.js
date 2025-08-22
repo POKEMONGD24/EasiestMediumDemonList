69f3eef8980d8e8ab9cf20f10bf0c855a4524caf
POKEMONGD24 <stojkovicrobin@gmail.com>
2025-08-23 01:19:30 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -4,8 +4,8 @@ import Roulette from './pages/Roulette.js';
 import ListPacks from './pages/ListPacks.js'
  
 export default [
-    { path: '/', component: List },
+    { path: '/list', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
-    { path: '/list-packs', component: ListPacks }
+    { path: '/list-packs', component: ListPacks },
 ];  
