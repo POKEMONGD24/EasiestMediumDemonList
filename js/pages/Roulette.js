2aeaab31ce06c2ff4f8d474945fa89d20c232f60
POKEMONGD24 <stojkovicrobin@gmail.com>
2025-08-22 23:52:26 +0200

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -1,6 +1,6 @@
 import { fetchList } from '../content.js';
 import { getThumbnailFromId, getYoutubeIdFromUrl, shuffle } from '../util.js';
- 
+
 import Spinner from '../components/Spinner.js';
 import Btn from '../components/Btn.js';
  
