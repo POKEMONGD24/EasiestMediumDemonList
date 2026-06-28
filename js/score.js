dd1b733baf2e2e658931dc07e4d904966c00f06e
huggies <hugni123@gmail.com>
2026-06-28 19:48:50 +1000

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,10 +1,10 @@
 const scale = 3;
 
 export function score(rank, percent, minPercent) {
-    if (rank > 150 && rank <= 253) {
+    if (rank > 150 && rank <= 257) {
         return 5.0;
     }
-    if (rank > 253) {
+    if (rank > 257) {
         return 2.0;
        }
     if (percent < 100) {
